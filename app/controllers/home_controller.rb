89ca6f011ab6c02c42de0b67ba7e917d3996f315
Mayank Saraf <mayankjadam74@gmail.com>
2023-04-20 17:09:28 +0530

    Presented fetched news data on index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @news = NewsApi.new(filter_params).get_news
+  end
+
+  private
+
+  def filter_params
+    params.permit(:category, :country, :language)
   end
 end
